5c4f5e141760d78d1b88d7467618930d5f5c5dea
A200134739 <psrvatka@gmail.com>
2022-07-13 21:38:21 +0200

    new

--- AAApracaTUKE/src/work13_07_2022/Palindrom2.java
+++ AAApracaTUKE/src/work13_07_2022/Palindrom2.java
@@ -3,20 +3,18 @@ package work13_07_2022;
 public class Palindrom2 {
     public static void main(String[] args) {
 
-        String veta = "Kobyla ma maly bok";
- //       System.out.println(veta);                   //vytlac povodnu vetu
+        String veta = "Kajak";
 
-        System.out.println("Je nasledujuci vyraz palindrom? "+veta+"\n"+comparePalindrom (veta));
-
+        System.out.println("Je nasledujuci vyraz palindrom? " + veta + "\n" + comparePalindrom(veta));
     }
 
-    private static String  comparePalindrom(String veta) {
+    private static String comparePalindrom(String veta) {
         for (int i = 0; i < veta.length(); i++) {
-            if((Character.toUpperCase(veta.charAt(i)))==(Character.toUpperCase(veta.charAt(veta.length()-i-1)))) {
-                return "Ano, toto je palindrom..";
+            if ((Character.toUpperCase(veta.charAt(i))) != (Character.toUpperCase(veta.charAt(veta.length() - i - 1)))) {
+                return "Nie, nie je to palindrom";
             }
         }
-        return "Nie, nie je to palindrom";
+        return "Ano, toto je palindrom..";
     }
 }
 
