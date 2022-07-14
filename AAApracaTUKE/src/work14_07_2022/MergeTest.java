ebffacbf2e57d98111ebe174f13e1d15ccf989a7
Peter_Srvatka <psrvatka@gmail.com>
2022-07-14 13:06:51 +0200

    asdf

--- AAApracaTUKE/src/work14_07_2022/MergeTest.java
+++ AAApracaTUKE/src/work14_07_2022/MergeTest.java
@@ -2,7 +2,7 @@ package work14_07_2022;
 
 public class MergeTest {
     public static void main(String[] args) {
-        System.out.println("hellloOuOu!");
+        System.out.println("hellloOuOu!zmena2");
     }
     
     // dalsia zmena
