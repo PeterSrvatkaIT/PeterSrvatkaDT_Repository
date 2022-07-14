28729a0390571ea5b8c61566590f0c8831838c89
PeterSrvatkaIT <psrvatka@gmail.com>
2022-07-14 13:00:34 +0200

    Update MergeTest.java

--- AAApracaTUKE/src/work14_07_2022/MergeTest.java
+++ AAApracaTUKE/src/work14_07_2022/MergeTest.java
@@ -2,7 +2,7 @@ package work14_07_2022;
 
 public class MergeTest {
     public static void main(String[] args) {
-        System.out.println("helllo!");
+        System.out.println("hellloOuOu!");
     }
     
     // dalsia zmena
