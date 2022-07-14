c47887af3643e80e9e14b2cd4fd3049dc3c2963b
Peter_Srvatka <psrvatka@gmail.com>
2022-07-14 15:12:15 +0200

    regex

---
+++ AAApracaTUKE/src/countries/Country.java
@@ -0,0 +1,9 @@
+package countries;
+
+public class Country {
+    private String shortName, countryName;
+    public Country(String shortName, String countryName) {
+        this.shortName= shortName;
+        this.countryName= countryName;
+    }
+}
